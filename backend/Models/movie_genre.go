4bbe7f1a2ac59d6c2ff701495eaebb05a18edbdb
dtxpder <1119753960@qq.com>
2022-03-03 14:47:05 -0500

    movie_genre

--- backend/Models/movie_genre.go
+++ backend/Models/movie_genre.go
@@ -1,10 +1,10 @@
 package Models
 
 type Movie_genre struct {
-	Movie_id   uint
-	Genre_id   uint
-	Movie      Movie      `gorm:"ForeignKey:Movie_id"` // 设置关联模型并指定UserId作为外键
-	Genre_list Genre_list `gorm:"ForeignKey:Genre_id"` // 设置关联模型并指定UserId作为外键
+	Movie_id uint
+	Genre_id uint
+	Movie    Movie `gorm:"ForeignKey:Movie_id"` // 设置关联模型并指定UserId作为外键
+	Genre    Genre `gorm:"ForeignKey:Genre_id"` // 设置关联模型并指定UserId作为外键
 }
 
 // 表名默认为模型名称的复数，此处手动设置User的表名为`users`
