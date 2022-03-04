ab5aa02913bbe38cdf292b933d85416fa497e439
dtxpder <1119753960@qq.com>
2022-03-04 16:22:23 -0500

    update the file of user_test

--- backend/test/user_test.go
+++ backend/test/user_test.go
@@ -12,3 +12,41 @@ func TestGetList(t *testing.T) {
 		t.Log("result is right")
 	}
 }
+
+func TestLogIn(t *testing.T) {
+	var user = Models.User{Username: "qwe", Password: "123"}
+	if Models.LogIn(user) == true {
+		t.Error("result is wrong!")
+	} else {
+		t.Log("result is right")
+	}
+}
+
+func TestSignUp(t *testing.T) {
+	var user = Models.User{Username: "zt", Password: "zt123", User_type: 0, Gender: "male", Age: 23}
+	if Models.SignUp(user) == 0 {
+		t.Error("result is wrong!")
+	} else {
+		t.Log("result is right")
+	}
+}
+
+func TestUpdateProfile(t *testing.T) {
+
+	var user = Models.User{Username: "zt", Password: "zt123", User_type: 0, Gender: "male", Age: 23}
+	if Models.UpdateProfile(user) == 0 {
+		t.Error("result is wrong!")
+	} else {
+		t.Log("result is right")
+	}
+
+}
+
+func TestDeleteUser(t *testing.T) {
+	//var user = Models.User{User_id: 11}
+	if Models.DeleteUser(11) == 0 {
+		t.Error("result is wrong!")
+	} else {
+		t.Log("result is right")
+	}
+}
