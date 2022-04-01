903fe9766e072297a9d882eddc71b1023065445c
dtxpder <1119753960@qq.com>
2022-04-01 12:40:07 -0400

    add backend test

--- backend/test/user_test.go
+++ backend/test/user_test.go
@@ -12,3 +12,68 @@ func TestGetList(t *testing.T) {
 		t.Log("result is right")
 	}
 }
+
+func TestDeleteUser(t *testing.T) {
+	if Models.DeleteUser(1) == 0 {
+		t.Error("result is wrong!")
+	} else {
+		t.Log("result is right")
+	}
+
+}
+
+func TestUpdateProfile(t *testing.T) {
+	var user = Models.User{User_id: 2, Username: "hhhh", Password: "123", User_type: 1, Gender: "s", Age: 2, Email: "123"}
+	if Models.UpdateProfile(user) == 0 {
+		t.Error("result is wrong!")
+	} else {
+		t.Log("result is right")
+	}
+}
+
+func TestSignUp(t *testing.T) {
+	var user = Models.User{User_id: 2, Username: "hhhh", Password: "123", User_type: 1, Gender: "s", Age: 2, Email: "123"}
+	//var review = Models.Review{Review_id: 2, Review_content: "lalalal", User_id: 11, Movie_id: 1}
+	if Models.SignUp(user) == 0 {
+		t.Error("result is wrong!")
+	} else {
+		t.Log("result is right")
+	}
+}
+
+func TestLogIn(t *testing.T) {
+	var user = Models.User{Username: "hhhh", Password: "123"}
+	resUser := Models.User{}
+	if Models.LogIn(user) == resUser {
+		t.Error("result is wrong!")
+	} else {
+		t.Log("result is right")
+	}
+}
+
+func TestQuery(t *testing.T) {
+	user := Models.User{}
+	if Models.QueryUserInfoByUserId(2) == user {
+		t.Error("result is wrong!")
+	} else {
+		t.Log("result is right")
+	}
+}
+
+//func UpdateCast(cast Cast) int64 {
+//	result := Database.DB.Model(&cast).Updates(&cast)
+//	//result := Database.DB.Save(&user)
+//	return result.RowsAffected
+//}
+//
+//func DeleteCast(Cast_id int) int64 {
+//	result := Database.DB.Delete(&Cast{}, Cast_id)
+//	return result.RowsAffected
+//}
+//
+//func SearchCast(castName string) []Cast {
+//	var cast []Cast
+//	//Database.DB.First(&cast, castName)
+//	Database.DB.Where("cast_name LIKE ?", "%"+castName+"%").Find(&cast)
+//	return cast
+//}
