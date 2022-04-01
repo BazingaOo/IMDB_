2f24e0f62443f79b1d90a534fb648af6c3b39dba
dtxpder <1119753960@qq.com>
2022-04-01 12:41:56 -0400

    update add function for movie_cast

--- backend/Models/movie_cast.go
+++ backend/Models/movie_cast.go
@@ -1,5 +1,7 @@
 package Models
 
+import "backend/Database"
+
 type Movie_cast struct {
 	Cast_id  uint
 	Movie_id uint
@@ -7,7 +9,13 @@ type Movie_cast struct {
 	Cast     Cast  `gorm:"ForeignKey:Cast_id"`  // 设置关联模型并指定UserId作为外键
 }
 
-// 表名默认为模型名称的复数，此处手动设置User的表名为`users`
-func (Movie_cast) TableName() string {
-	return "movie_cast"
+//// 表名默认为模型名称的复数，此处手动设置User的表名为`users`
+//func (Movie_cast) TableName() string {
+//	return "movie_cast"
+//}
+
+func AddMovieCast(movieCast Movie_cast) int64 {
+	result := Database.DB.Create(&movieCast) // 通过数据的指针来创建
+	affected := result.RowsAffected          // 返回插入记录的条数
+	return affected
 }
