1232551ec2ce67a2f13494f27a1c8bb6e93b5d65
dtxpder <1119753960@qq.com>
2022-04-19 16:16:59 -0400

    add rating test

---
+++ backend/test/rating_test.go
@@ -0,0 +1,51 @@
+package Test
+
+import (
+	"backend/Models"
+	"testing"
+)
+
+func TestAddRating(t *testing.T) {
+	var rating = Models.Rating{Movie_id: 1, User_id: 2, Score: 1}
+	if Models.AddRating(rating) == 0 {
+		t.Error("result is wrong!")
+	} else {
+		t.Log("result is right")
+	}
+}
+
+func TestDeleteRating(t *testing.T) {
+
+	var rating = Models.Rating{User_id: 2, Movie_id: 3}
+	if Models.DeleteRating(rating) == 0 {
+		t.Error("result is wrong!")
+	} else {
+		t.Log("result is right")
+	}
+}
+
+func TestReadRating(t *testing.T) {
+	//var user_id = 1
+	if Models.ReadRating(2) == nil {
+		t.Error("result is wrong!")
+	} else {
+		t.Log("result is right")
+	}
+}
+
+func TestUpdateRating(t *testing.T) {
+	var rating = Models.Rating{User_id: 1, Movie_id: 3, Score: 4}
+	if Models.UpdateRating(rating) == 0 {
+		t.Error("result is wrong!")
+	} else {
+		t.Log("result is right")
+	}
+}
+
+func TestUpdateGrade(t *testing.T) {
+	if Models.UpdateGrade(3) == 0 {
+		t.Error("result is wrong!")
+	} else {
+		t.Log("result is right")
+	}
+}
