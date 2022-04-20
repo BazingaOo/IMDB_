6c668f44cb064b1fe7aa82db3f2a5833f1a02474
wangyx98 <wangyx9863@126.com>
2022-04-20 00:55:25 -0400

    add more in the database

--- IMDB.sql
+++ IMDB.sql
@@ -68,6 +68,14 @@ CREATE TABLE `genre`  (
 -- ----------------------------
 -- Records of genre
 -- ----------------------------
+INSERT INTO `genre` VALUES (1, 'Comedy');
+INSERT INTO `genre` VALUES (2, 'Thriller');
+INSERT INTO `genre` VALUES (3, 'Action');
+INSERT INTO `genre` VALUES (4, 'Scientific-fiction');
+INSERT INTO `genre` VALUES (5, 'Adventure');
+INSERT INTO `genre` VALUES (6, 'Romantic');
+INSERT INTO `genre` VALUES (7, 'Documentary');
+INSERT INTO `genre` VALUES (8, 'Drama');
 
 -- ----------------------------
 -- Table structure for movie
@@ -126,15 +134,6 @@ CREATE TABLE `movie_cast`  (
 INSERT INTO `movie_cast` VALUES (1, 1);
 INSERT INTO `movie_cast` VALUES (2, 1);
 INSERT INTO `movie_cast` VALUES (3, 5);
-INSERT INTO `movie_cast` VALUES (3, 5);
-INSERT INTO `movie_cast` VALUES (3, 5);
-INSERT INTO `movie_cast` VALUES (3, 5);
-INSERT INTO `movie_cast` VALUES (3, 5);
-INSERT INTO `movie_cast` VALUES (3, 5);
-INSERT INTO `movie_cast` VALUES (3, 5);
-INSERT INTO `movie_cast` VALUES (3, 5);
-INSERT INTO `movie_cast` VALUES (3, 5);
-
 
 -- ----------------------------
 -- Table structure for movie_genre
@@ -152,6 +151,26 @@ CREATE TABLE `movie_genre`  (
 -- ----------------------------
 -- Records of movie_genre
 -- ----------------------------
+INSERT INTO `movie_genre` VALUES (1, 4);
+INSERT INTO `movie_genre` VALUES (2, 8);
+INSERT INTO `movie_genre` VALUES (3, 4);
+INSERT INTO `movie_genre` VALUES (4, 1);
+INSERT INTO `movie_genre` VALUES (5, 2);
+INSERT INTO `movie_genre` VALUES (6, 4);
+INSERT INTO `movie_genre` VALUES (7, 8);
+INSERT INTO `movie_genre` VALUES (8, 8);
+INSERT INTO `movie_genre` VALUES (9, 8);
+INSERT INTO `movie_genre` VALUES (10, 4);
+INSERT INTO `movie_genre` VALUES (11, 4);
+INSERT INTO `movie_genre` VALUES (12, 8);
+INSERT INTO `movie_genre` VALUES (13, 8);
+INSERT INTO `movie_genre` VALUES (14, 4);
+INSERT INTO `movie_genre` VALUES (15, 4);
+INSERT INTO `movie_genre` VALUES (16, 8);
+INSERT INTO `movie_genre` VALUES (17, 8);
+INSERT INTO `movie_genre` VALUES (18, 8);
+INSERT INTO `movie_genre` VALUES (19, 6);
+INSERT INTO `movie_genre` VALUES (20, 7);
 
 -- ----------------------------
 -- Table structure for rating
@@ -170,7 +189,128 @@ CREATE TABLE `rating`  (
 -- ----------------------------
 -- Records of rating
 -- ----------------------------
-INSERT INTO `rating` VALUES (NULL, 1, '5');
+INSERT INTO `rating` VALUES (1, 1, '5');
+INSERT INTO `rating` VALUES (1, 5, '4.5');
+INSERT INTO `rating` VALUES (1, 2, '5');
+INSERT INTO `rating` VALUES (1, 2, '4.5');
+INSERT INTO `rating` VALUES (1, 3, '4.3');
+INSERT INTO `rating` VALUES (1, 4, '3.8');
+INSERT INTO `rating` VALUES (1, 5, '2.1');
+INSERT INTO `rating` VALUES (1, 6, '2.5');
+INSERT INTO `rating` VALUES (1, 7, '3.8');
+INSERT INTO `rating` VALUES (1, 8, '4.6');
+INSERT INTO `rating` VALUES (1, 9, '4.9');
+INSERT INTO `rating` VALUES (1, 10, '5.0');
+INSERT INTO `rating` VALUES (1, 11, '1.2');
+INSERT INTO `rating` VALUES (1, 12, '3.7');
+INSERT INTO `rating` VALUES (1, 13, '1.4');
+INSERT INTO `rating` VALUES (1, 14, '3.6');
+INSERT INTO `rating` VALUES (1, 15, '4.6');
+INSERT INTO `rating` VALUES (1, 16, '2,7');
+INSERT INTO `rating` VALUES (1, 17, '3.4');
+INSERT INTO `rating` VALUES (1, 18, '5.0');
+INSERT INTO `rating` VALUES (1, 19, '4.2');
+INSERT INTO `rating` VALUES (1, 20, '1.4');
+INSERT INTO `rating` VALUES (2, 11, '4,6');
+INSERT INTO `rating` VALUES (2, 2, '4.6');
+INSERT INTO `rating` VALUES (2, 3, '2,7');
+INSERT INTO `rating` VALUES (2, 15, '4.4');
+INSERT INTO `rating` VALUES (2, 5, '5.0');
+INSERT INTO `rating` VALUES (2, 6, '4.2');
+INSERT INTO `rating` VALUES (2, 7, '4.2');
+INSERT INTO `rating` VALUES (2, 8, '3.6');
+INSERT INTO `rating` VALUES (2, 9, '4.2');
+INSERT INTO `rating` VALUES (2, 10, '4.1');
+INSERT INTO `rating` VALUES (2, 14, '4.5');
+INSERT INTO `rating` VALUES (2, 12, '4.9');
+INSERT INTO `rating` VALUES (2, 13, '4.3');
+INSERT INTO `rating` VALUES (2, 1, '3.6');
+INSERT INTO `rating` VALUES (2, 4, '3.4');
+INSERT INTO `rating` VALUES (2, 16, '4.2');
+INSERT INTO `rating` VALUES (2, 17, '3.2');
+INSERT INTO `rating` VALUES (2, 18, '3.1');
+INSERT INTO `rating` VALUES (2, 19, '2.8');
+INSERT INTO `rating` VALUES (2, 20, '3.7');
+INSERT INTO `rating` VALUES (3, 12, '3.2');
+INSERT INTO `rating` VALUES (3, 13, '3.5');
+INSERT INTO `rating` VALUES (3, 3, '2.1');
+INSERT INTO `rating` VALUES (3, 14, '3.0');
+INSERT INTO `rating` VALUES (3, 5, '1.6');
+INSERT INTO `rating` VALUES (3, 17, '3.4');
+INSERT INTO `rating` VALUES (3, 7, '1.0');
+INSERT INTO `rating` VALUES (3, 8, '2.5');
+INSERT INTO `rating` VALUES (3, 9, '5.0');
+INSERT INTO `rating` VALUES (3, 10, '3.8');
+INSERT INTO `rating` VALUES (3, 11, '3.5');
+INSERT INTO `rating` VALUES (3, 4, '4.5');
+INSERT INTO `rating` VALUES (3, 2, '2.0');
+INSERT INTO `rating` VALUES (3, 16, '3.8');
+INSERT INTO `rating` VALUES (3, 15, '3.7');
+INSERT INTO `rating` VALUES (3, 1, '4.6');
+INSERT INTO `rating` VALUES (3, 6, '1.4');
+INSERT INTO `rating` VALUES (3, 18, '3.6');
+INSERT INTO `rating` VALUES (3, 19, '3.8');
+INSERT INTO `rating` VALUES (3, 20, '3.0');
+INSERT INTO `rating` VALUES (4, 1, '3.9');
+INSERT INTO `rating` VALUES (4, 2, '3,5');
+INSERT INTO `rating` VALUES (4, 3, '4.5');
+INSERT INTO `rating` VALUES (4, 4, '4.3');
+INSERT INTO `rating` VALUES (4, 5, '3.8');
+INSERT INTO `rating` VALUES (4, 6, '2.1');
+INSERT INTO `rating` VALUES (4, 7, '2.5');
+INSERT INTO `rating` VALUES (4, 8, '3.8');
+INSERT INTO `rating` VALUES (4, 9, '4.6');
+INSERT INTO `rating` VALUES (4, 10, '4.9');
+INSERT INTO `rating` VALUES (4, 11, '5.0');
+INSERT INTO `rating` VALUES (4, 12, '1.2');
+INSERT INTO `rating` VALUES (4, 13, '3.7');
+INSERT INTO `rating` VALUES (4, 14, '1.4');
+INSERT INTO `rating` VALUES (4, 15, '3.6');
+INSERT INTO `rating` VALUES (4, 16, '4.6');
+INSERT INTO `rating` VALUES (4, 17, '2,7');
+INSERT INTO `rating` VALUES (4, 18, '3.4');
+INSERT INTO `rating` VALUES (4, 19, '5.0');
+INSERT INTO `rating` VALUES (4, 20, '4.2');
+INSERT INTO `rating` VALUES (5, 1, '1.4');
+INSERT INTO `rating` VALUES (5, 2, '3.6');
+INSERT INTO `rating` VALUES (5, 3, '4.6');
+INSERT INTO `rating` VALUES (5, 4, '2,7');
+INSERT INTO `rating` VALUES (5, 5, '3.4');
+INSERT INTO `rating` VALUES (5, 6, '5.0');
+INSERT INTO `rating` VALUES (5, 7, '4.5');
+INSERT INTO `rating` VALUES (5, 8, '4.3');
+INSERT INTO `rating` VALUES (5, 9, '3.8');
+INSERT INTO `rating` VALUES (5, 10, '2.1');
+INSERT INTO `rating` VALUES (5, 11, '2.5');
+INSERT INTO `rating` VALUES (5, 12, '3.8');
+INSERT INTO `rating` VALUES (5, 13, '4.6');
+INSERT INTO `rating` VALUES (5, 14, '4.9');
+INSERT INTO `rating` VALUES (5, 15, '5.0');
+INSERT INTO `rating` VALUES (5, 16, '1.2');
+INSERT INTO `rating` VALUES (5, 17, '3.7');
+INSERT INTO `rating` VALUES (5, 18, '1.4');
+INSERT INTO `rating` VALUES (5, 19, '3.6');
+INSERT INTO `rating` VALUES (5, 20, '4.6');
+INSERT INTO `rating` VALUES (6, 1, '2,7');
+INSERT INTO `rating` VALUES (6, 2, '3.4');
+INSERT INTO `rating` VALUES (6, 3, '5.0');
+INSERT INTO `rating` VALUES (6, 4, '4.2');
+INSERT INTO `rating` VALUES (6, 5, '1.4');
+INSERT INTO `rating` VALUES (6, 6, '3.6');
+INSERT INTO `rating` VALUES (6, 7, '4.6');
+INSERT INTO `rating` VALUES (6, 8, '2,7');
+INSERT INTO `rating` VALUES (6, 9, '3.4');
+INSERT INTO `rating` VALUES (6, 10, '4.5');
+INSERT INTO `rating` VALUES (6, 11, '4.3');
+INSERT INTO `rating` VALUES (6, 12, '3.8');
+INSERT INTO `rating` VALUES (6, 13, '2.1');
+INSERT INTO `rating` VALUES (6, 14, '2.5');
+INSERT INTO `rating` VALUES (6, 15, '3.8');
+INSERT INTO `rating` VALUES (6, 16, '4.6');
+INSERT INTO `rating` VALUES (6, 17, '4.9');
+INSERT INTO `rating` VALUES (6, 18, '5.0');
+INSERT INTO `rating` VALUES (6, 19, '1.2');
+INSERT INTO `rating` VALUES (6, 20, '3.7');
 
 -- ----------------------------
 -- Table structure for review
@@ -191,7 +331,26 @@ CREATE TABLE `review`  (
 -- ----------------------------
 -- Records of review
 -- ----------------------------
-INSERT INTO `review` VALUES (1, NULL, 1, 'This movie is good.');
+INSERT INTO `review` VALUES (1, 1, 1, 'This movie is good.');
+INSERT INTO `review` VALUES (2, 1, 15, 'I like this movie very much.');
+INSERT INTO `review` VALUES (3, 1, 7, 'I do not like this movie.');
+INSERT INTO `review` VALUES (4, 2, 4, 'I appreciate the actors\' performance.');
+INSERT INTO `review` VALUES (5, 2, 18, 'This movie is worthy in watching. ');
+INSERT INTO `review` VALUES (6, 2, 14, 'This movie is a trash.');
+INSERT INTO `review` VALUES (7, 3, 11, 'This movie should get an award.');
+INSERT INTO `review` VALUES (8, 4, 20, 'This movie is wonderful.');
+INSERT INTO `review` VALUES (9, 5, 12, 'I have never watched such an amazing film.');
+INSERT INTO `review` VALUES (10, 6, 17, 'This film is awesome.');
+INSERT INTO `review` VALUES (11, 6, 6, 'Enjoy watching this movie.');
+INSERT INTO `review` VALUES (12, 6, 3, 'Dislike. Worst cast!');
+INSERT INTO `review` VALUES (13, 3, 2, 'This movie makes my cry!');
+INSERT INTO `review` VALUES (14, 3, 5, 'This movie is really funny!');
+INSERT INTO `review` VALUES (15, 4, 8, 'I love the cast so much!');
+INSERT INTO `review` VALUES (16, 4, 9, 'Woo-hoo! Big heart to this movie!');
+INSERT INTO `review` VALUES (17, 5, 10, 'Love this movie!');
+INSERT INTO `review` VALUES (18, 5, 13, 'Trash! Worst!');
+INSERT INTO `review` VALUES (19, 3, 16, 'Hate this storyline!');
+INSERT INTO `review` VALUES (20, 5, 19, 'Not funny! I donnot wanna watch it again.');
 
 -- ----------------------------
 -- Table structure for user
@@ -211,8 +370,12 @@ CREATE TABLE `user`  (
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES (1, 'qwe', '123', NULL, NULL, NULL, NULL);
-INSERT INTO `user` VALUES (2, 'qwe2', '123456', NULL, 'female', 12, '123@qwe.com');
+INSERT INTO `user` VALUES (1, 'qwe', '123', 1, 'male', 19, 'erer@gmail.com');
+INSERT INTO `user` VALUES (2, 'qwe2', '123456', 2, 'female', 12, '123@qwe.com');
+INSERT INTO `user` VALUES (3, 'wangyx', '13579', 3, 'male', 23, 'wangyx@gmail.com');
+INSERT INTO `user` VALUES (4, 'blessedroll', '246810', 2, 'male', 29, 'blessedroll@gmail.com');
+INSERT INTO `user` VALUES (5, 'dtxpder', '19283', 1, 'female', 23, 'dtxpder@gmail.com');
+INSERT INTO `user` VALUES (6, 'bazingaoo', '234667', 1, 'male', 23, 'bazingaoo@gmail.com');
 
 -- ----------------------------
 -- Table structure for watch_list
@@ -230,5 +393,25 @@ CREATE TABLE `watch_list`  (
 -- ----------------------------
 -- Records of watch_list
 -- ----------------------------
+INSERT INTO `watch_list` VALUES (1, 2);
+INSERT INTO `watch_list` VALUES (1, 15);
+INSERT INTO `watch_list` VALUES (1, 7);
+INSERT INTO `watch_list` VALUES (2, 4);
+INSERT INTO `watch_list` VALUES (2, 18);
+INSERT INTO `watch_list` VALUES (2, 14);
+INSERT INTO `watch_list` VALUES (3, 11);
+INSERT INTO `watch_list` VALUES (4, 20);
+INSERT INTO `watch_list` VALUES (5, 12);
+INSERT INTO `watch_list` VALUES (6, 17);
+INSERT INTO `watch_list` VALUES (6, 6);
+INSERT INTO `watch_list` VALUES (6, 3);
+INSERT INTO `watch_list` VALUES (3, 2);
+INSERT INTO `watch_list` VALUES (3, 5);
+INSERT INTO `watch_list` VALUES (4, 8);
+INSERT INTO `watch_list` VALUES (4, 9);
+INSERT INTO `watch_list` VALUES (5, 10);
+INSERT INTO `watch_list` VALUES (5, 13);
+INSERT INTO `watch_list` VALUES (3, 16);
+INSERT INTO `watch_list` VALUES (5, 19);
 
 SET FOREIGN_KEY_CHECKS = 1;
