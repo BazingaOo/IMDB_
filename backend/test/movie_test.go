5f1211287b867c21a0a0c59cac26ac28ec870768
dtxpder <1119753960@qq.com>
2022-04-20 03:33:35 -0400

    update movie test

--- backend/test/movie_test.go
+++ backend/test/movie_test.go
@@ -1,87 +1,172 @@
 package Test
 
 import (
-	"backend/Models"
-	"fmt"
+	genreController "backend/Controllers"
+	movieController "backend/Controllers"
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"net/url"
+	"strings"
 	"testing"
 )
 
-func TestSearchMovieByCast(t *testing.T) {
-	var castName = ""
-	if Models.SearchMovieByCast(castName) == nil {
-		t.Error("result is wrong!")
-	} else {
-		t.Log("result is right")
-	}
-}
+//func TestSearchMovieWithGenre(t *testing.T) {
+//	//var castName = ""
+//	if Models.SearchMovieWithGenre(1) == nil {
+//		t.Error("result is wrong!")
+//	} else {
+//		t.Log("result is right")
+//	}
+//}
+
 func TestSearchMovieByName(t *testing.T) {
-	var name = "2"
-	if Models.SearchMovieByName(name) == nil {
-		t.Error("result is wrong!")
-	} else {
-		fmt.Println(Models.SearchMovieByName(name))
-		t.Log("11")
+
+	router := gin.New()
+
+	movieGroup := router.Group("/user/movie")
+	{
+		movieGroup.POST("/searchMovieByName", movieController.SearchMovieByName)
+		movieGroup.POST("/searchMovieByCast", movieController.SearchMovieByCast)
+		movieGroup.POST("/searchMovieByMovieId", movieController.SearchMovieByMovieId)
+		movieGroup.POST("/searchMovieWithGenre", genreController.SearchMovieWithGenre)
+		movieGroup.POST("/searchMovieByYear", movieController.SearchMovieByYear)
+		movieGroup.POST("/topMovie", movieController.TopMovie)
 	}
+
+	params := url.Values{}
+	params.Add("searchName", "test")
+	para1 := params.Encode()
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest("POST", "/user/movie/searchMovieByName", strings.NewReader(para1))
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	router.ServeHTTP(w, req)
+	assert.Equal(t, 200, w.Code)
+
 }
 
-func TestAddMovie(t *testing.T) {
-	movie := Models.Movie{
-		Movie_id:    4,
-		Movie_name:  "test",
-		Year:        1998,
-		Description: "awesome!"}
-	if Models.AddMovie(movie) == 0 {
-		t.Error("result is wrong!")
-	} else {
-		t.Log("result is right")
+func TestSearchMovieByCast(t *testing.T) {
+
+	router := gin.New()
+
+	movieGroup := router.Group("/user/movie")
+	{
+		movieGroup.POST("/searchMovieByName", movieController.SearchMovieByName)
+		movieGroup.POST("/searchMovieByCast", movieController.SearchMovieByCast)
+		movieGroup.POST("/searchMovieByMovieId", movieController.SearchMovieByMovieId)
+		movieGroup.POST("/searchMovieWithGenre", genreController.SearchMovieWithGenre)
+		movieGroup.POST("/searchMovieByYear", movieController.SearchMovieByYear)
+		movieGroup.POST("/topMovie", movieController.TopMovie)
 	}
+
+	params2 := url.Values{}
+	params2.Add("castName", "Tao")
+	para2 := params2.Encode()
+	w2 := httptest.NewRecorder()
+	req2, _ := http.NewRequest("POST", "/user/movie/searchMovieByCast", strings.NewReader(para2))
+	req2.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	router.ServeHTTP(w2, req2)
+	assert.Equal(t, 200, w2.Code)
+
 }
 
-func TestUpdateMovie(t *testing.T) {
-	movie := Models.Movie{
-		Movie_id:    4,
-		Movie_name:  "test",
-		Year:        2001,
-		Description: "awesome!"}
-	if Models.UpdateMovie(movie) == 0 {
-		t.Error("result is wrong!")
-	} else {
-		t.Log("result is right")
+func TestSearchMovieByMovieId(t *testing.T) {
+
+	router := gin.New()
+
+	movieGroup := router.Group("/user/movie")
+	{
+		movieGroup.POST("/searchMovieByName", movieController.SearchMovieByName)
+		movieGroup.POST("/searchMovieByCast", movieController.SearchMovieByCast)
+		movieGroup.POST("/searchMovieByMovieId", movieController.SearchMovieByMovieId)
+		movieGroup.POST("/searchMovieWithGenre", genreController.SearchMovieWithGenre)
+		movieGroup.POST("/searchMovieByYear", movieController.SearchMovieByYear)
+		movieGroup.POST("/topMovie", movieController.TopMovie)
 	}
+
+	params3 := url.Values{}
+	params3.Add("movieId", "1")
+	para3 := params3.Encode()
+	w3 := httptest.NewRecorder()
+	req3, _ := http.NewRequest("POST", "/user/movie/searchMovieByMovieId", strings.NewReader(para3))
+	req3.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	router.ServeHTTP(w3, req3)
+	assert.Equal(t, 200, w3.Code)
+
+}
+
+func TestSearchMovieWithGenre(t *testing.T) {
+
+	router := gin.New()
+
+	movieGroup := router.Group("/user/movie")
+	{
+		movieGroup.POST("/searchMovieByName", movieController.SearchMovieByName)
+		movieGroup.POST("/searchMovieByCast", movieController.SearchMovieByCast)
+		movieGroup.POST("/searchMovieByMovieId", movieController.SearchMovieByMovieId)
+		movieGroup.POST("/searchMovieWithGenre", genreController.SearchMovieWithGenre)
+		movieGroup.POST("/searchMovieByYear", movieController.SearchMovieByYear)
+		movieGroup.POST("/topMovie", movieController.TopMovie)
+	}
+
+	params4 := url.Values{}
+	params4.Add("movieId", "1")
+	para4 := params4.Encode()
+	w4 := httptest.NewRecorder()
+	req4, _ := http.NewRequest("POST", "/user/movie/searchMovieWithGenre", strings.NewReader(para4))
+	req4.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	router.ServeHTTP(w4, req4)
+	assert.Equal(t, 200, w4.Code)
+
 }
 
 func TestSearchMovieByYear(t *testing.T) {
-	var year = 2021
-	//fmt.Println(Models.SearchMovieByMovieId(5))
-	//movie := Models.Movie{}
-	if Models.SearchMovieByYear(year) == nil {
-		t.Error("result is wrong!")
-	} else {
-		t.Log("result is right")
-	}
-}
 
-func TestDeleteMovie(t *testing.T) {
-	if Models.DeleteMovie(1) == 0 {
-		t.Error("result is wrong!")
-	} else {
-		t.Log("result is right")
+	router := gin.New()
+
+	movieGroup := router.Group("/user/movie")
+	{
+		movieGroup.POST("/searchMovieByName", movieController.SearchMovieByName)
+		movieGroup.POST("/searchMovieByCast", movieController.SearchMovieByCast)
+		movieGroup.POST("/searchMovieByMovieId", movieController.SearchMovieByMovieId)
+		movieGroup.POST("/searchMovieWithGenre", movieController.SearchMovieWithGenre)
+		movieGroup.POST("/searchMovieByYear", movieController.SearchMovieByYear)
+		movieGroup.POST("/topMovie", movieController.TopMovie)
 	}
+
+	params5 := url.Values{}
+	params5.Add("year", "2021")
+	para5 := params5.Encode()
+	w5 := httptest.NewRecorder()
+	req5, _ := http.NewRequest("POST", "/user/movie/searchMovieByYear", strings.NewReader(para5))
+	req5.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	router.ServeHTTP(w5, req5)
+	assert.Equal(t, 200, w5.Code)
+
 }
 
 func TestTopMovie(t *testing.T) {
-	if Models.TopMovie() == nil {
-		t.Error("result is wrong!")
-	} else {
-		t.Log("result is right")
-	}
-}
 
-func TestAddMovieCast(t *testing.T) {
-	var movieCast = Models.Movie_cast{Cast_id: 2, Movie_id: 3}
-	if Models.AddMovieCast(movieCast) == 0 {
-		t.Error("result is wrong!")
-	} else {
-		t.Log("result is right")
+	router := gin.New()
+
+	movieGroup := router.Group("/user/movie")
+	{
+		movieGroup.POST("/searchMovieByName", movieController.SearchMovieByName)
+		movieGroup.POST("/searchMovieByCast", movieController.SearchMovieByCast)
+		movieGroup.POST("/searchMovieByMovieId", movieController.SearchMovieByMovieId)
+		movieGroup.POST("/searchMovieWithGenre", genreController.SearchMovieWithGenre)
+		movieGroup.POST("/searchMovieByYear", movieController.SearchMovieByYear)
+		movieGroup.POST("/topMovie", movieController.TopMovie)
 	}
+
+	params5 := url.Values{}
+	//params5.Add("year", "2021")
+	para5 := params5.Encode()
+	w5 := httptest.NewRecorder()
+	req5, _ := http.NewRequest("POST", "/user/movie/topMovie", strings.NewReader(para5))
+	req5.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	router.ServeHTTP(w5, req5)
+	assert.Equal(t, 200, w5.Code)
+
 }
